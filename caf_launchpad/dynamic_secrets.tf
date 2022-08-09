424cc93724e3406a244976b79e317758db3433f7
Mathias Lipke <mathias.lipke.ext@miele.com>
2022-08-09 13:52:12 +0200

    Adjust to INFOMOTION Repo

--- caf_launchpad/dynamic_secrets.tf
+++ caf_launchpad/dynamic_secrets.tf
@@ -1,6 +1,12 @@
 
 module "dynamic_keyvault_secrets" {
-  source = "../aztfmod/modules/security/dynamic_keyvault_secrets"
+  
+  # source  = "aztfmod/caf/azurerm//modules/security/dynamic_keyvault_secrets"
+  # version = "5.5.5"
+  
+  #source = "../aztfmod/modules/security/dynamic_keyvault_secrets"
+
+  source = "git::https://github.com/INFOMOTION/terraform-azurerm-caf.git/modules/security/dynamic_keyvault_secrets?ref=5.6.0-patch"
 
   for_each = try(var.dynamic_keyvault_secrets, {})
 
--- caf_solution/dynamic_secrets.tf
+++ caf_solution/dynamic_secrets.tf
@@ -1,5 +1,7 @@
 module "dynamic_keyvault_secrets" {
-  source = "../aztfmod/modules/security/dynamic_keyvault_secrets"
+  #source = "../aztfmod/modules/security/dynamic_keyvault_secrets"
+
+  source = "git::https://github.com/INFOMOTION/terraform-azurerm-caf.git/modules/security/dynamic_keyvault_secrets?ref=5.6.0-patch"
 
   for_each = {
     for keyvault_key, secrets in try(var.dynamic_keyvault_secrets, {}) : keyvault_key => {
