7ba318f4a3fd68d8f7106f002f27712b053ec618
Mathias Lipke <mathias.lipke.ext@miele.com>
2022-08-26 23:50:05 +0200

    Add Output

--- caf_solution/add-ons/databricks_v2/output.tf
+++ caf_solution/add-ons/databricks_v2/output.tf
@@ -4,3 +4,9 @@ output "clusters" {
 output "groups" {
   value = databricks_group.groups
 }
+output "service_principals" {
+  value = databricks_service_principal.sp
+}
+output "managed_identities" {
+  value = databricks_service_principal.msi
+}
