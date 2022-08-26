0e05991186cbdfc088ee743a7f31ac9dc8f3242b
Mathias Lipke <mathias.lipke.ext@miele.com>
2022-08-27 01:31:35 +0200

    sensitive = true

--- caf_solution/add-ons/databricks_v2/output.tf
+++ caf_solution/add-ons/databricks_v2/output.tf
@@ -6,7 +6,9 @@ output "groups" {
 }
 output "service_principals" {
   value = databricks_service_principal.sp
+  sensitive = true
 }
 output "managed_identities" {
   value = databricks_service_principal.msi
+  sensitive = true
 }
